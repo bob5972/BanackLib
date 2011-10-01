69a7dcc964f16edf1de2153bebc44eb04433bd59
Michael Banack <bob5972@banack.net>
2011-10-01 15:22:04 -0700

    Rewrote Debug

--- net/banack/debug/Debug.java
+++ net/banack/debug/Debug.java
@@ -24,285 +24,212 @@ import java.io.PrintWriter;
 
 import net.banack.util.Stack;
 
-public class Debug
-{
-	private static boolean DEBUG=false;
-	private static boolean MESSAGES=false;
-	public static boolean CRASH_ON_ERROR=false;
-	
-	public static boolean STACK_TRACE_ON_VERBOSE=false;//LOTS OF OUTPUT!
-	public static boolean STACK_TRACE_ON_INFO=false;
-	public static boolean STACK_TRACE_ON_WARN=true;
-	public static boolean STACK_TRACE_ON_ERROR=true;
-	
-	
-	public static boolean STD_OUT_MESSAGES=false;
-	public static boolean STD_OUT_MESSAGES_VERBOSE=false;
-	public static boolean STD_OUT_MESSAGES_INFO=false;
-	public static boolean STD_OUT_MESSAGES_WARN=false;
-	public static boolean STD_OUT_MESSAGES_ERROR=false;
-	
-	public static boolean STD_ERR_MESSAGES=false;
-	public static boolean STD_ERR_MESSAGES_VERBOSE=false;
-	public static boolean STD_ERR_MESSAGES_INFO=false;
-	public static boolean STD_ERR_MESSAGES_WARN=true;
-	public static boolean STD_ERR_MESSAGES_ERROR=true;
-	
-	private static String LOG_FILE = "debug.log";
-
-	
-	public static boolean LOG_MESSAGES=false;
-	public static boolean LOG_MESSAGES_VERBOSE=false;
-	public static boolean LOG_MESSAGES_INFO=true;
-	public static boolean LOG_MESSAGES_WARN=true;
-	public static boolean LOG_MESSAGES_ERROR=true;
-	
-	private static PrintWriter logOut;
-	
-	private static Stack<Long>  timerStack = new Stack<Long>();
-	
-	
-	
-	public static void enableDebug()
-	{
-		DEBUG=true;
-	}
-	
-	public static void setDebug(boolean b)
-	{
-		DEBUG = b;
-	}
-	
-	public static void disableDebug()
-	{
-		DEBUG=false;
-	}
-	
-	public static void enableMessages()
-	{
-		MESSAGES=true;
-	}
-	
-	public static void setMessages(boolean b)
-	{
-		MESSAGES=b;
-	}
-	
-	public static void disableMessages()
-	{
-		MESSAGES=false;
-	}
-	
-	public static boolean isDebug()
-	{
-		return DEBUG;
-	}
-	
-	public static boolean showMessages()
-	{
-		return MESSAGES;
-	}
-	
-	public static boolean showWarnings()
-	{
-		return LOG_MESSAGES_WARN || STD_ERR_MESSAGES_WARN || STD_OUT_MESSAGES_WARN;
-	}
-	
-	public static boolean showErrors()
-	{
-		return LOG_MESSAGES_ERROR || STD_ERR_MESSAGES_ERROR || STD_OUT_MESSAGES_ERROR;
-	}
-	
-	public static boolean showInfo()
-	{
-		return LOG_MESSAGES_INFO || STD_ERR_MESSAGES_INFO || STD_OUT_MESSAGES_INFO;
-	}
-	
-	
-	public static void stdOutPrint(String msg)
-	{
-		if(!MESSAGES)
-			return;
-		if(STD_OUT_MESSAGES)
-		{
-			System.out.println(msg);
-		}
-	}
-	
-	public static void stdErrPrint(String msg)
-	{
-		if(!MESSAGES)
-			return;
-		if(STD_ERR_MESSAGES)
-			System.err.println(msg);
-	}
-	
-	public static void logPrint(String msg)
-	{
-		if(!MESSAGES)
-			return;
-		if(LOG_MESSAGES)
-		{
-			if(logOut==null)
-			{
-				try {
-					logOut = new PrintWriter(new FileOutputStream(LOG_FILE));
-				}
-				catch(FileNotFoundException e)
-				{
-					System.err.println("Unable to initialize debug log!");
-					e.printStackTrace();
-					System.exit(1);
-				}
-			}
-			
-			logOut.println(msg);
-		}
-	}
-	
-	public static void info(Object message)
-	{
-		if(!MESSAGES)
-			return;
-		String oup = message.toString();
-		if(STD_OUT_MESSAGES_INFO)
-			stdOutPrint(oup);
-		if(STD_ERR_MESSAGES_INFO)
-			stdErrPrint(oup);
-		if(LOG_MESSAGES_INFO)
-			logPrint(oup);
-		
-		if(STACK_TRACE_ON_INFO)
-			printStackTrace();
-	}
-	
-	public static void verbose(Object message)
-	{
-		if(!MESSAGES)
-			return;
-		String oup = message.toString();
-		if(STD_OUT_MESSAGES_VERBOSE)
-			stdOutPrint(oup);
-		if(STD_ERR_MESSAGES_VERBOSE)
-			stdErrPrint(oup);
-		if(LOG_MESSAGES_VERBOSE)
-			logPrint(oup);
-		
-		if(STACK_TRACE_ON_VERBOSE)
-			printStackTrace();
-	}
-	
-	public static void warn(Object message)
-	{
-		if(!MESSAGES)
-			return;
-		String oup = message.toString();
-		if(STD_OUT_MESSAGES_WARN)
-			stdOutPrint(oup);
-		if(STD_ERR_MESSAGES_WARN)
-			stdErrPrint(oup);
-		if(LOG_MESSAGES_WARN)
-			logPrint(oup);
-		
-		if(STACK_TRACE_ON_WARN)
-			printStackTrace();
-	}
-	
-	public static void error(Object message)
-	{
-		if(!MESSAGES)
-			return;
-		String oup = message.toString();
-		
-		if(STD_OUT_MESSAGES_ERROR)
-			stdOutPrint(oup);
-		if(STD_ERR_MESSAGES_ERROR)
-			stdErrPrint(oup);
-		if(LOG_MESSAGES_ERROR)
-			logPrint(oup);
-		
-		if(STACK_TRACE_ON_ERROR)
-			printStackTrace();
-		
-		if(CRASH_ON_ERROR)
-		{
-			crash();
-		}
-	}
-	
-	public static void printStackTrace()
-	{
-		Exception e = new Exception();
-		printStackTrace(e);
-	}
-	
-	public static void printStackTrace(Exception e)
-	{
-		if(STD_OUT_MESSAGES)
-			e.printStackTrace(System.out);
-		if(STD_ERR_MESSAGES)
-			e.printStackTrace(System.err);
-		if(LOG_MESSAGES)
-			e.printStackTrace(logOut);
-		
-	}
-	
-	public static void crash()
-	{
-		crash(new Exception());
-	}
-	
-	public static void crash(String msg)
-	{
-		crash(new Exception(msg));
-	}
-	
-	public static void crash(Exception e, String msg)
-	{
-		print(msg);
-		crash(e);
-	}
-	
-	public static void print(String msg)
-	{
-		stdOutPrint(msg);
-		stdErrPrint(msg);
-		logPrint(msg);
-	}
-	
-	public static void crash(Exception e)
-	{
-		print("Crashing!");
-		String msg = e.getMessage();
-		
-		if(msg == null)
-			msg = "";
-		msg = "Fatal Error"+((msg.equals(""))?("!"):(": "+msg));
-		
-		print(msg);
-		printStackTrace(e);
-		
-		if(!STD_OUT_MESSAGES && !STD_ERR_MESSAGES && !LOG_MESSAGES)
-		{
-			System.err.println(msg);
-			e.printStackTrace();			
-		}
-		
-		System.exit(1);
-	}
-	
-	public static void pushTime()
-	{
-		timerStack.push(System.currentTimeMillis());
-	}
-	
-	public static long getElapsedTime()
-	{
-		return System.currentTimeMillis()-timerStack.top();
-	}
-	
-	public static long popElapsedTime()
-	{
-		return System.currentTimeMillis()-timerStack.pop();
-	}
+public class Debug {
+    private static boolean DEBUG = false;
+    
+    public static class MessageOptions {
+        public boolean stdOut;
+        public boolean stdErr;
+        public boolean log;
+        
+        public MessageOptions(boolean out, boolean err, boolean log)
+        {
+            this.stdOut = out;
+            this.stdErr = err;
+            this.log = log;
+        }
+    }
+    
+    public static MessageOptions printOptions;
+    public static MessageOptions warningOptions;
+    public static MessageOptions logOptions;
+    public static MessageOptions panicOptions;
+    
+    /*
+     * Initialize message options.
+     */
+    static {
+        enableMessages();
+    }
+    
+    private static String LOG_FILE = "debug.log";
+    
+    private static PrintWriter logOut;
+    private static Stack<Long> timerStack = new Stack<Long>();
+    
+    public static void setMessageDefaults()
+    {
+        
+    }
+    
+    public static void enableDebug()
+    {
+        DEBUG = true;
+    }
+    
+    public static void setDebug(boolean b)
+    {
+        DEBUG = b;
+    }
+    
+    public static void disableDebug()
+    {
+        DEBUG = false;
+    }
+    
+    public static boolean isDebug()
+    {
+        return DEBUG;
+    }
+    
+    public static void enableMessages()
+    {
+        printOptions = new MessageOptions(true, false, true);
+        warningOptions = new MessageOptions(false, true, true);
+        logOptions = new MessageOptions(false, false, true);
+        panicOptions = new MessageOptions(false, true, true);
+    }
+    
+    public static void disableMessages()
+    {
+        printOptions = new MessageOptions(false, false, false);
+        warningOptions = new MessageOptions(false, true, false);
+        logOptions = new MessageOptions(false, false, true);
+        panicOptions = new MessageOptions(false, true, true);
+    }
+    
+    
+    public static void setMessages(boolean b)
+    {
+        if (b) {
+            enableMessages();
+        } else {
+            disableMessages();
+        }
+    }
+    
+    
+    public static void stdOutPrint(String msg)
+    {
+        System.out.println(msg);
+    }
+    
+    public static void stdErrPrint(String msg)
+    {
+        System.err.println(msg);
+    }
+    
+    private static void initLog()
+    {
+        if (logOut == null) {
+            try {
+                logOut = new PrintWriter(new FileOutputStream(LOG_FILE));
+            }
+            catch (FileNotFoundException e) {
+                System.err.println("Unable to initialize debug log!");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+    
+    public static void logPrint(String msg)
+    {
+        initLog();
+        
+        logOut.println(msg);
+        logOut.flush();
+    }
+    
+    public static void printMessage(MessageOptions opt, String msg)
+    {
+        if (opt.stdOut) {
+            stdOutPrint(msg);
+        }
+        
+        if (opt.stdErr) {
+            stdErrPrint(msg);
+        }
+        
+        if (opt.log) {
+            logPrint(msg);
+        }
+    }
+    
+    
+    public static void warning(Object message)
+    {
+        String oup = message.toString();
+        
+        printMessage(warningOptions, oup);
+    }
+    
+    public static void printStackTrace(MessageOptions opt, Exception e)
+    {
+        if (opt.stdOut) {
+            e.printStackTrace(System.out);
+        }
+        
+        if (opt.stdErr) {
+            e.printStackTrace(System.err);
+        }
+        
+        if (opt.log) {
+            initLog();
+            e.printStackTrace(logOut);
+        }
+        
+    }
+    
+    public static void panic()
+    {
+        panic(new Exception());
+    }
+    
+    public static void panic(String msg)
+    {
+        panic(new Exception(msg));
+    }
+    
+    public static void panic(Exception e, String msg)
+    {
+        msg = "Panic" + ((msg.equals("")) ? ("!\n") : (": " + msg));
+        printMessage(panicOptions, msg);
+        panic(e);
+    }
+    
+    public static void print(String msg)
+    {
+        printMessage(printOptions, msg);
+    }
+    
+    public static void panic(Exception e)
+    {
+        String msg = e.getMessage();
+        
+        if (msg == null) {
+            msg = "";
+        }
+        msg = "Panic" + ((msg.equals("")) ? ("!\n") : (": " + msg));
+        
+        print(msg);
+        printStackTrace(panicOptions, e);
+        
+        System.exit(1);
+    }
+    
+    public static void pushTime()
+    {
+        timerStack.push(System.currentTimeMillis());
+    }
+    
+    public static long getElapsedTime()
+    {
+        return System.currentTimeMillis() - timerStack.top();
+    }
+    
+    public static long popElapsedTime()
+    {
+        return System.currentTimeMillis() - timerStack.pop();
+    }
 }
